5fe01785946f4e171289e94eae0a347695527e28
TreyWehrmeyer <treywehr1@gmail.com>
2020-12-20 17:40:15 -0600

    Adding code

--- Mapping_GeoJSON_Polygons/static/js/logic.js
+++ Mapping_GeoJSON_Polygons/static/js/logic.js
@@ -78,7 +78,7 @@ let streets = L.tileLayer('https://api.mapbox.com/styles/v1/mapbox/streets-v11/t
 });
 
 // We create the dark view tile layer that will be an option for our map.
-let dark = L.tileLayer('https://api.mapbox.com/styles/v1/mapbox/dark-v10/tiles/{z}/{x}/{y}?access_token={accessToken}', {
+let satelliteStreets = L.tileLayer('https://api.mapbox.com/styles/v1/mapbox/satellite-streets-v11/tiles/{z}/{x}/{y}?access_token={accessToken}', {
     attribution: 'Map data © <a href="https://www.openstreetmap.org/">OpenStreetMap</a> contributors, <a href="https://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, Imagery (c) <a href="https://www.mapbox.com/">Mapbox</a>',
     maxZoom: 18,
     accessToken: API_KEY
@@ -86,30 +86,33 @@ let dark = L.tileLayer('https://api.mapbox.com/styles/v1/mapbox/dark-v10/tiles/{
 
 // Create a base layer that holds both maps.
 let baseMaps = {
-    Street: streets,
-    Dark: dark
+    "Street": streets,
+    "Satellite Streets": satelliteStreets
 };
 
 // Create the map object with center, zoom level and default layer.
 let map = L.map('mapid', {
-    center: [30, 30],
-    zoom: 2,
-    layers: [streets]
+    center: [43.7, -79.3],
+    zoom: 11,
+    layers: [satelliteStreets]
 })
 
 // Pass our map layers into our layers control and add the layers control to the map.
 L.control.layers(baseMaps).addTo(map);
 
-let airportData = "https://raw.githubusercontent.com/treywehr1/Mapping_Earthquakes/main/majorAirports.json"
+let torontoHoods = "https://raw.githubusercontent.com/treywehr1/Mapping_Earthquakes/main/torontoNeighborhoods.json"
 
 // Grabbing our GeoJSON data.
-d3.json(airportData).then(function (data) {
+d3.json(torontoHoods).then(function (data) {
     console.log(data);
     // Creating a GeoJSON layer with the retrieved data.
     L.geoJSON(data, {
+        color: "blue",
+        weight: 1,
+        fillColor: "yellow",
         onEachFeature: function (feature, layer) {
             console.log(layer);
-            layer.bindPopup("<h2>" + "Airport Code: " + feature.properties.faa + "</h2>" + "<hr></hr>" + "<h3>" + feature.properties.city + ", " + feature.properties.country + "</h3");
+            layer.bindPopup("<h2>" + "Neighborhood: " + feature.properties.AREA_NAME + "</h2>" + "<hr></hr>" + "<h3>" + feature.properties.AREA_S_CD + ", " + feature.properties.country + "</h3");
         }
     }).addTo(map);
 });
