62f46bcb26edc7ed3eb2e640e8850fce7424f7d0
TreyWehrmeyer <treywehr1@gmail.com>
2020-12-20 18:40:49 -0600

    Adding Polygon data

--- Earthquakes_past7days/static/js/logicStep1.js
+++ Earthquakes_past7days/static/js/logicStep1.js
@@ -92,27 +92,16 @@ let baseMaps = {
 
 // Create the map object with center, zoom level and default layer.
 let map = L.map('mapid', {
-    center: [43.7, -79.3],
-    zoom: 11,
+    center: [39.5, -98.5],
+    zoom: 3,
     layers: [satelliteStreets]
 })
 
 // Pass our map layers into our layers control and add the layers control to the map.
 L.control.layers(baseMaps).addTo(map);
 
-let torontoHoods = "https://raw.githubusercontent.com/treywehr1/Mapping_Earthquakes/main/torontoNeighborhoods.json"
-
-// Grabbing our GeoJSON data.
-d3.json(torontoHoods).then(function (data) {
-    console.log(data);
+// Retrieve the earthquake GeoJSON data.
+d3.json("https://earthquake.usgs.gov/earthquakes/feed/v1.0/summary/all_week.geojson").then(function (data) {
     // Creating a GeoJSON layer with the retrieved data.
-    L.geoJSON(data, {
-        color: "blue",
-        weight: 1,
-        fillColor: "yellow",
-        onEachFeature: function (feature, layer) {
-            console.log(layer);
-            layer.bindPopup("<h2>" + "Neighborhood: " + feature.properties.AREA_NAME + "</h2>" + "<hr></hr>" + "<h3>" + feature.properties.AREA_S_CD + ", " + feature.properties.country + "</h3");
-        }
-    }).addTo(map);
+    L.geoJson(data).addTo(map);
 });
